f0f3e788ef13d6bb8d2dca142fa3af085a21f331
Jahongir Ergashev <jahongirergawev2@gmail.com>
2023-02-08 14:48:45 +0500

    salom

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
+const TOKEN = "6099013146:AAGOyIUp38iq9xRej7E4pUQmBJ4VSwU8Oo4";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://web-telegram-app.vercel.app/";
 
 bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+  ctx.reply("Welcome : my book store bot", {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
