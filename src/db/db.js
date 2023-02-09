b7b4e508257698279532eea6ec22be6c0dc479c5
Jahongir Ergashev <jahongirergawev2@gmail.com>
2023-02-09 21:26:38 +0500

    yangi rasmlar qo'shildi

--- src/db/db.js
+++ src/db/db.js
@@ -1,19 +1,18 @@
-import pizzaImg from "../images/pizza.png";
-import burgerImg from "../images/burger.png";
-import cocaImg from "../images/coca.png";
-import saladImg from "../images/salad.png";
-import waterImg from "../images/water.png";
-import iceCreamImg from "../images/icecream.png";
-import kebabImg from "../images/kebab.png";
+
+import image1 from "../images/1.jpg";
+import image2 from "../images/2.jpg";
+import image3 from "../images/3.jpg";
+import image4 from "../images/4.jpg";
+import image5 from "../images/5.jpg";
+import image6 from "../images/6.jpg";
 
 export function getData() {
   return [
-    { title: "Pizza", price: 17.99, Image: pizzaImg,id:1 },
-    { title: "Burger", price: 15, Image: burgerImg,id:2 },
-    { title: "Coca", price: 3.5, Image: cocaImg ,id:3},
-    { title: "Kebab", price: 13.99, Image: kebabImg,id:4 },
-    { title: "Salad", price: 2.5, Image: saladImg,id:5 },
-    { title: "Bottle of water", price: 0.99, Image: waterImg,id:6 },
-    { title: "Ice cream", price: 2.99, Image: iceCreamImg,id:7 },
+    { title: "S19J pro 100TH", price: 15.60, Image: image1,id:1 },
+    { title: "S19 95TH ", price: 11.5, Image: image2,id:2 },
+    { title: "S19 95Th/s", price: 11.5, Image: image3 ,id:3},
+    { title: "WHATSMINER M30S++", price: 16, Image: image4,id:4 },
+    { title: "S19 100Th/s", price: 19, Image: image5,id:5 },
+    { title: "S19j PRO 104Th/s", price:  21, Image: image6,id:6 },
   ];
 }
