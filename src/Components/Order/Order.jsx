d6e65d66c08d91d6ba9f16dd7f6ea7b06f1632a6
Jahongir Ergashev <jahongirergawev2@gmail.com>
2023-03-02 17:42:11 +0500

    oreder componentda o'zgarishlar qilindi

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,8 +1,213 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import swal from "sweetalert";
+import Select from "react-select";
 import { getMainer } from "../../Api/Axios";
+import allbrand from "../../images/allbarnd.webp";
+import bitmain from "../../images/bitmain.webp";
+import canaan from "../../images/canaan.webp";
+import whatsminer from "../../images/whatsminer.webp";
+import count1040 from "../../images/10-40count.webp";
+import count50100 from "../../images/50-100count.webp";
+import count100 from "../../images/100count.webp";
 import "./order.css";
+
+const barndOption = [
+    {
+        value: "ALL BRAND",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={allbrand}
+                    alt="allbarnd"
+                    width={200}
+                    style={{ marginRight: 10, width: 200, height: 40 }}
+                />
+                <span>ALL BRAND</span>
+            </span>
+        ),
+    },
+    {
+        value: "WHATSMINER",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={whatsminer}
+                    alt="whatsminer"
+                    style={{ marginRight: 10, width: 200, height: 40 }}
+                />
+                <span>WHATSMINER</span>
+            </span>
+        ),
+    },
+    {
+        value: "BITMAIN",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={bitmain}
+                    alt="bitmain"
+                    width={200}
+                    style={{ marginRight: 10, width: 200, height: 40 }}
+                />
+                <span>BITMAIN</span>
+            </span>
+        ),
+    },
+    {
+        value: "CANAAN",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={canaan}
+                    alt="canaan"
+                    width={200}
+                    style={{ marginRight: 10, width: 200, height: 40 }}
+                />
+                <span>CANAAN</span>
+            </span>
+        ),
+    },
+];
+
+const countOption = [
+    {
+        value: "MOQ 10-40 ",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={count1040}
+                    alt="count 10-40"
+                    width={200}
+                    style={{ marginRight: 10, width: 70, height: 50 }}
+                />
+                <span>MOQ 10-40 </span>
+            </span>
+        ),
+    },
+    {
+        value: "MOQ 50-100",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={count50100}
+                    alt="count 50-100"
+                    width={200}
+                    style={{ marginRight: 10, width: 70, height: 50 }}
+                />
+                <span>MOQ 50-100</span>
+            </span>
+        ),
+    },
+    {
+        value: "MOQ 100+",
+        label: (
+            <span
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    color: "#000",
+                    fontSize: 18,
+                }}
+            >
+                <img
+                    src={count100}
+                    alt="count 100+"
+                    width={200}
+                    style={{ marginRight: 10, width: 70, height: 50 }}
+                />
+                <span>MOQ 100+</span>
+            </span>
+        ),
+    },
+];
+
+const locationOption = [
+    {
+        value: "USA",
+        label: "USA",
+    },
+    {
+        value: "CANADA",
+        label: "CANADA",
+    },
+    {
+        value: "ASIA",
+        label: "ASIA",
+    },
+    {
+        value: "EUROPA",
+        label: "EUROPA",
+    },
+    {
+        value: "LATIN AMERICA",
+        label: "LATIN AMERICA",
+    },
+];
+
+const conditionOption = [
+    {
+        value: "NEW",
+        label: "NEW",
+    },
+    {
+        value: "USED",
+        label: "USED",
+    },
+    {
+        value: "I DON’T MIND",
+        label: "I DON’T MIND",
+    },
+];
 
 const Order = ({ setUserId, setMainer }) => {
     const [state, setState] = useState({
@@ -47,19 +252,19 @@ const Order = ({ setUserId, setMainer }) => {
     };
 
     const onChangeBrand = (e) => {
-        setState((prev) => ({ ...prev, brandSelect: e.target.value }));
+        setState((prev) => ({ ...prev, brandSelect: e.value }));
     };
 
     const onChangeCount = (e) => {
-        setState((prev) => ({ ...prev, countSelect: e.target.value }));
+        setState((prev) => ({ ...prev, countSelect: e.value }));
     };
 
     const onChangeLocation = (e) => {
-        setState((prev) => ({ ...prev, locationSelect: e.target.value }));
+        setState((prev) => ({ ...prev, locationSelect: e.value }));
     };
 
     const onChangeCondition = (e) => {
-        setState((prev) => ({ ...prev, conditionSelect: e.target.value }));
+        setState((prev) => ({ ...prev, conditionSelect: e.value }));
     };
 
     return (
@@ -71,63 +276,51 @@ const Order = ({ setUserId, setMainer }) => {
                         What’s type of manufacture brand are you currently
                         looking for?
                     </label>
-                    <select
+                    <Select
+                        className="basic-single"
+                        classNamePrefix="select"
+                        defaultValue={barndOption[0]}
                         name="brandSelect"
+                        options={barndOption}
                         onChange={onChangeBrand}
                         required
-                    >
-                        <option value="ALL BRAND">ALL BRAND</option>
-                        <option value="BITMAIN">BITMAIN</option>
-                        <option value="WHATSMINER">WHATSMINER</option>
-                        <option value="CANAAN">CANAAN</option>
-                    </select>
+                    />
                     <label htmlFor="countSelect">
                         What size of order are you looking at?
                     </label>
-                    <select
+                    <Select
+                        className="basic-single"
+                        classNamePrefix="select"
+                        defaultValue={countOption[0]}
+                        options={countOption}
                         name="countSelect"
                         onChange={onChangeCount}
                         required
-                    >
-                        <option value="10-">
-                            10 <span>&#60;</span>
-                        </option>
-                        <option value="50-">
-                            50 <span>&#60;</span>
-                        </option>
-                        <option value="100-">
-                            100 <span>&#60;</span>
-                        </option>
-                        <option value="100+">
-                            100 <span>&#62;</span>
-                        </option>
-                    </select>
+                    />
                     <label htmlFor="locationSelect">
                         What location you prefer the units to be located?
                     </label>
-                    <select
+                    <Select
+                        className="basic-single"
+                        classNamePrefix="select"
+                        defaultValue={locationOption[0]}
+                        options={locationOption}
                         name="locationSelect"
                         onChange={onChangeLocation}
                         required
-                    >
-                        <option value="USA">USA</option>
-                        <option value="CANADA">CANADA</option>
-                        <option value="ASIA">ASIA</option>
-                        <option value="EUROPA">EUROPA</option>
-                        <option value="LATIN AMERICA">LATIN AMERICA</option>
-                    </select>
+                    />
                     <label htmlFor="conditionSelect">
                         In what condition would you prefer the units?
                     </label>
-                    <select
+                    <Select
+                        className="basic-single"
+                        classNamePrefix="select"
+                        defaultValue={conditionOption[0]}
+                        options={conditionOption}
                         name="conditionSelect"
                         onChange={onChangeCondition}
                         required
-                    >
-                        <option value="NEW">NEW</option>
-                        <option value="USED">USED</option>
-                        <option value="I DON’T MIND">I DON’T MIND</option>
-                    </select>
+                    />
                     <button type="submit">Submit</button>
                 </form>
             </div>
